781e46661039c1397752daa19ec28d921c7855ac
felipemonteiro <eu.felipemonteirobr@gmail.com>
2026-05-26 22:17:07 -0300

    Configurei o relacionemento Eloquent

--- backend/app/Models/Alunos.php
+++ backend/app/Models/Alunos.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Alunos extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'Cursos_id'
+    ];
+
+    public function Cursos()
+    {
+        return $this->belongsTo(Cursos::class);
+    }
 }
--- backend/app/Models/Cursos.php
+++ backend/app/Models/Cursos.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Cursos extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description'
+    ];
+
+    public function Alunos()
+    {
+        return $this->hasMany(Alunos::class);
+    }
 }
+//
