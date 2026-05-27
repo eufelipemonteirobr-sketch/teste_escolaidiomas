77397d2c492acb33c7ec3b886a709e8302a55bc8
felipemonteiro <eu.felipemonteirobr@gmail.com>
2026-05-27 17:50:21 -0300

    fix: configure student relationship

---
+++ backend/app/Models/Student.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Course;
+
+class Student extends Model
+{
+    protected $fillable = [
+        'name',
+        'email',
+        'phone',
+        'course_id'
+    ];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+}
